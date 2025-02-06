b6add058111fb15f44f09db63a69b90c08c4c4aa
Hector Acevedo <HEA092024@gmail.com>
2025-02-06 09:49:21 +0100

    .

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -1,7 +1,11 @@
 const DishCard = () => {
-    return (
-        <div id="frame">
-
-        </div>
-    )
-}
+  return (
+    <div id="frame">
+      <p>Id</p>
+      <h2>Tittel</h2>
+      <p>Ingredienser</p>
+      <p>Pris</p>
+      <p>Kategori</p>
+    </div>
+  );
+};
